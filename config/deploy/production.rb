e94359705ff243ea454626599c32bf866077d9f0
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-07-17 00:37:54 +0200

    fixes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '35.180.205.58', user: 'ubuntu', roles: %w{app db web}
+server '13.37.32.243', user: 'ubuntu', roles: %w{app db web}
 
 set :branch, 'main'
 

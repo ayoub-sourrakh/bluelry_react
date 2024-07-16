11a51f9fd75cc74272d74f2c996e0b8b148dc129
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-07-16 18:30:24 +0200

    config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, "git@github.com:ayoub-sourrakh/bluelry_react.git"
 
 # Définition du chemin par défaut, y compris le chemin de node
 set :default_env, {
-  path: "/home/ubuntu/.nvm/versions/node/v#{fetch(:node_version)}/bin:$PATH"
+path: "/home/ubuntu/.nvm/versions/node/v#{fetch(:node_version)}/bin:$PATH"
 }
 
 set :deploy_to, '/var/www/bluelry_react'
@@ -16,29 +16,31 @@ set :deploy_to, '/var/www/bluelry_react'
 append :linked_dirs, 'node_modules', 'log', 'public/system', 'tmp/cache', 'tmp/pids', 'tmp/sockets', 'vendor/bundle', 'public/build'
 
 namespace :deploy do
-  desc 'Install npm dependencies'
-  task :npm_install do
-    on roles(:app) do
-      within release_path do
-        execute :bash, "-l -c 'source ~/.nvm/nvm.sh && nvm use #{fetch(:node_version)} && npm install --production'"
-      end
+    desc 'Install npm dependencies'
+    task :npm_install do
+        on roles(:app) do
+            within release_path do
+                execute :bash, "-l -c 'source ~/.nvm/nvm.sh && nvm use #{fetch(:node_version)} && npm install --production'"
+            end
+        end
     end
-  end
-  
-  desc 'Build React app'
-  task :build_react do
-    on roles(:app) do
-      within release_path do
-        execute :bash, "-l -c 'source ~/.nvm/nvm.sh && nvm use #{fetch(:node_version)} && npm run build'"
-      end
+    
+    desc 'Force reinstall react-scripts and build React app'
+    task :build_react do
+        on roles(:app) do
+            within release_path do
+                # Assurez-vous que react-scripts est installé
+                execute :bash, "-l -c 'source ~/.nvm/nvm.sh && nvm use #{fetch(:node_version)} && npm install react-scripts@latest && npm run build'"
+            end
+        end
     end
-  end
-  
-  after :publishing, :build_react
+    
+    after :publishing, :build_react
 end
 
+
 set :ssh_options, {
-  forward_agent: true,
-  auth_methods: %w[publickey],
-  user: 'ubuntu'
+forward_agent: true,
+auth_methods: %w[publickey],
+user: 'ubuntu'
 }

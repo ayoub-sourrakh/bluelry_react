414168855e81065fa23737cdcfd32772d93067b8
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-07-16 18:28:33 +0200

    config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,67 +5,40 @@ set :node_version, '14.21.3'
 set :application, "bluelry"
 set :repo_url, "git@github.com:ayoub-sourrakh/bluelry_react.git"
 
-# Setting the default environment to include the Node.js path
+# Définition du chemin par défaut, y compris le chemin de node
 set :default_env, {
   path: "/home/ubuntu/.nvm/versions/node/v#{fetch(:node_version)}/bin:$PATH"
 }
 
 set :deploy_to, '/var/www/bluelry_react'
 
-# Configuration des tâches de déploiement
+# Liens vers des dossiers qui doivent être persistants entre les déploiements
+append :linked_dirs, 'node_modules', 'log', 'public/system', 'tmp/cache', 'tmp/pids', 'tmp/sockets', 'vendor/bundle', 'public/build'
+
 namespace :deploy do
-    desc 'Install npm dependencies'
-    task :npm_install do
-      on roles(:app) do
-        within release_path do
-          # Wrap the command to ensure it runs in a login shell
-          execute :bash, "-l -c 'source ~/.nvm/nvm.sh && nvm use #{fetch(:node_version)} && npm install --production'"
-        end
-      end
-    end
-    
-    desc 'Build React app'
-    task :build_react do
-      on roles(:app) do
-        within release_path do
-          # Wrap the command to ensure it runs in a login shell
-          execute :bash, "-l -c 'source ~/.nvm/nvm.sh && nvm use #{fetch(:node_version)} && npm run build'"
-        end
+  desc 'Install npm dependencies'
+  task :npm_install do
+    on roles(:app) do
+      within release_path do
+        execute :bash, "-l -c 'source ~/.nvm/nvm.sh && nvm use #{fetch(:node_version)} && npm install --production'"
       end
     end
   end
+  
+  desc 'Build React app'
+  task :build_react do
+    on roles(:app) do
+      within release_path do
+        execute :bash, "-l -c 'source ~/.nvm/nvm.sh && nvm use #{fetch(:node_version)} && npm run build'"
+      end
+    end
+  end
+  
+  after :publishing, :build_react
+end
 
-# Custom SSH options
 set :ssh_options, {
   forward_agent: true,
   auth_methods: %w[publickey],
   user: 'ubuntu'
 }
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure

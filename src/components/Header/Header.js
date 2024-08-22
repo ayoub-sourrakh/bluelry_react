2c13923a0b69791f7d34ed9fefa7f22de5e27d18
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-08-22 04:36:16 +0200

    fix header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import { Navbar, Nav, Container, Dropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
 import Logo from '../Logo/Logo';
+import './Header.css';
 
 const Header = () => {
   const { isAuthenticated, logout } = useContext(AuthContext);
@@ -15,21 +16,21 @@ const Header = () => {
           <Logo />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarResponsive" />
-        <Navbar.Collapse id="navbarResponsive">
+        <Navbar.Collapse id="navbarResponsive" className='navbar-collapse'>
           <Nav className="ms-auto">
             <Nav.Link as={Link} to="/catalogue">
-              <span style={{ display: 'flex', gap: '5px', alignItems: 'center', color: '#004AAD', fontSize: '1.2rem' }}>
+              <span style={{ display: 'flex', gap: '5px', alignItems: 'center', color: 'white', fontSize: '1.1rem' }}>
                 <i className="fas fa-book-open"></i> Catalogue
               </span>
             </Nav.Link>
             <Nav.Link as={Link} to="/cart">
-              <span style={{ display: 'flex', gap: '5px', alignItems: 'center', color: '#004AAD', fontSize: '1.2rem' }}>
+              <span style={{ display: 'flex', gap: '5px', alignItems: 'center', color: 'white', fontSize: '1.1rem' }}>
                 <i className="fas fa-shopping-cart"></i> Panier
               </span>
             </Nav.Link>
             {isAuthenticated ? (
               <Dropdown align="end">
-                <Dropdown.Toggle variant="dark" id="dropdown-basic" style={{ color: '#004AAD', fontSize: '1.2rem', display: 'flex', alignItems: 'center', gap: '5px' }}>
+                <Dropdown.Toggle variant="dark" id="dropdown-basic" style={{ color: 'white', fontSize: '1.1rem', display: 'flex', alignItems: 'center', gap: '5px' }}>
                   <i className="fas fa-user"></i> {user.firstName}
                 </Dropdown.Toggle>
 
@@ -40,7 +41,7 @@ const Header = () => {
               </Dropdown>
             ) : (
               <Nav.Link as={Link} to="/login">
-                <span style={{ display: 'flex', gap: '5px', alignItems: 'center', color: '#004AAD', fontSize: '1.2rem' }}>
+                <span style={{ display: 'flex', gap: '5px', alignItems: 'center', color: 'white', fontSize: '1.1rem' }}>
                   <i className="fas fa-sign-in-alt"></i> Connexion
                 </span>
               </Nav.Link>

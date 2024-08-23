cc0d67556110f758d3dcb7cd64f29bdb41c29223
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-08-23 04:18:03 +0200

    fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,39 +20,36 @@ const Header = () => {
         <Navbar.Brand as={Link} to="/" className="ms-3">
           <Logo />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarResponsive" />
-        <Navbar.Collapse id="navbarResponsive" className="navbar-collapse">
           <Nav className="ms-auto">
             <Nav.Link as={Link} to="/catalogue">
               <span style={{ display: 'flex', gap: '5px', alignItems: 'center', color: '#B5CAE6' }}>
-                <i className="fas fa-book-open"></i> Catalogue
+                <i className="fas fa-book-open"></i>
               </span>
             </Nav.Link>
             <Nav.Link as={Link} to="/cart">
               <span style={{ display: 'flex', gap: '5px', alignItems: 'center', color: '#B5CAE6' }}>
-                <i className="fas fa-shopping-cart"></i> Panier
+                <i className="fas fa-shopping-cart"></i>
               </span>
             </Nav.Link>
             {isAuthenticated && user ? (
               <Dropdown align="end">
                 <Dropdown.Toggle variant="dark" id="dropdown-basic" style={{ color: '#B5CAE6', display: 'flex', alignItems: 'center', gap: '5px' }}>
-                  <i className="fas fa-user"></i> {user.first_name}
+                  <i className="fas fa-user"></i>
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu className="dropdown-menu-dark">
-                  <Dropdown.Item as={Link} to="/profile">Mon Profil</Dropdown.Item>
+                  <Dropdown.Item as={Link} to="/profile">Profil</Dropdown.Item>
                   <Dropdown.Item onClick={handleLogout}>Déconnexion</Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
             ) : (
               <Nav.Link as={Link} to="/login">
                 <span style={{ display: 'flex', gap: '5px', alignItems: 'center', color: '#B5CAE6' }}>
-                  <i className="fas fa-sign-in-alt"></i> Connexion
+                  <i className="fas fa-sign-in-alt"></i>
                 </span>
               </Nav.Link>
             )}
           </Nav>
-        </Navbar.Collapse>
       </Container>
     </Navbar>
   );

3ca340df282f5855e88f1edbf328406a739e7af4
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2025-06-13 18:52:58 +0200

    fix capistrano deployment config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '13.37.32.243', user: 'ubuntu', roles: %w{app db web}
+server '13.39.110.124', user: 'ubuntu', roles: %w{app db web}
 
 set :branch, 'main'
 
